9ab8f3867da127ab1eaa381dace9d5cbed4c795e
hxx-ny <x.hia712@gmail.com>
2023-12-06 15:23:38 +0900

    .

--- PC/hand.py
+++ PC/hand.py
@@ -1,14 +1,34 @@
-import sys
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 
-cap = cv2.VideoCapture(0)
+cap = None
+detect = None
 
-while True:
+def setup():
+    global cap
+    cap = cv2.VideoCapture(0)
+    detect = HandDetector(maxHands=1, detectionCon=0.8)
+
+def toDo(frame):
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret = detect.findHands(frame)
+    print(ret)
+    return frame
+
+def loop():
     ret, frame = cap.read()
-    cv2.imshow('Camera', frame)
+    frame = toDo(frame)
+    cv2.inshow('Camera', frame)
 
-    key = chr(cv2.waitKey(1) & 0xFF)
+    key = chr(cv2.waitKey(1) & 0xFF).lower()
     if key == 'q':
         cap.release()
-        cv2.destoryAllWindows()
-        break
+        cv2.destroyAllWindows()
+
+def main():
+    setup()
+    while True:
+        loop()
+
+if __name__ == '__main__':
+    main()
